c1a97973e8201e06aceb8b2709bbf00c001aa882
neilcapri <neil.mukharji@konsciouskitchen.com>
2026-09-08 19:37:30 -0400

    Update Sourcing.jsx

--- src/pages/Sourcing.jsx
+++ src/pages/Sourcing.jsx
@@ -878,7 +878,7 @@ export function Reports() {
     a.click()
   }
 
-  const PACK_SIZE = { VPB:3,VPCAN:3,PNF:3,PVBRG:1,PVBR:4,PBB:2,PCC:2,KLR:2,KSCD:4,VPBD:2,KHD:2,HPCo:5,KABIS:5,KAB:5,KWAL:5,PVHC:5,POS:5,PGCo:5,KCOC:1,KSCO:5,PVBB:1,GBL:1,KPL:1,CCL:1,BAGL:2,Focaccia:1,TRFCS:1,HRCS:1,VSCS:1,NALCOB:1,NBFB:1,PRMC:1,CMC:1,LMC:1 }
+  const PACK_SIZE = { VPB:3,VPCAN:3,PNF:3,PVBRG:1,PVBR:4,PBB:2,PCC:2,KLR:2,KSCD:4,VPBD:2,KHD:2,HPCo:5,KABIS:5,KAB:5,KWAL:5,PVHC:5,POS:5,PGCo:5,KCOC:1,KSCo:5,PVBB:1,GBL:1,KPL:1,CCL:1,BAGL:2,Focaccia:1,TRFCS:1,HRCS:1,VSCS:1,NALCOB:1,NBFB:1,PRMC:1,CMC:1,LMC:1 }
 
   const tabStyle = (key) => ({
     padding: '10px 20px', border: 'none', background: 'none', cursor: 'pointer',
